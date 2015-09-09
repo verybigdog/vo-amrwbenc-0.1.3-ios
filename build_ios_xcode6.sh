ff85714a3cbdca206a1dc6f780af65d02b6ea0f7
tisquare <tisquare@tisuareui-MacBook-Pro.local>
2015-09-09 16:42:53 +0900

    script modify

--- build_ios_xcode6.sh
+++ build_ios_xcode6.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-modified from http://blog.csdn.net/favormm/article/details/6772097
+###modified from http://blog.csdn.net/favormm/article/details/6772097
 set -xe
 
 DEVELOPER=`xcode-select -print-path`
-DEST=`pwd .`"/vo-amrwb-enc"
+DEST=`pwd .`"/vo-amrwbenc"
 
 ARCHS="x86_64 armv7 armv7s arm64"
-LIBS="libopencore-amrnb.a libopencore-amrwb.a"
+LIBS="libvo-amrwbenc.a"
 # Note that AMR-NB is for narrow band http://en.wikipedia.org/wiki/Adaptive_Multi-Rate_audio_codec
 # for AMR-WB encoding, refer to http://sourceforge.net/projects/opencore-amr/files/vo-amrwbenc/
 # or AMR Codecs as Shared Libraries http://www.penguin.cz/~utx/amr
@@ -27,6 +27,7 @@ for arch in $ARCHS; do
                 echo "Building opencore-amr for iPhoneOS $arch ****************"
                 PATH=`xcodebuild -version -sdk iphoneos PlatformPath`"/Developer/usr/bin:$PATH" \
                 SDK=`xcodebuild -version -sdk iphoneos Path` \
+		CC="xcrun --sdk iphoneos clang -arch $arch $IOSMV --sysroot=$SDK -isystem $SDK/usr/include"\
                 CXX="xcrun --sdk iphoneos clang++ -arch $arch $IOSMV --sysroot=$SDK -isystem $SDK/usr/include" \
                 LDFLAGS="-Wl,-syslibroot,$SDK" \
                 ./configure \
@@ -37,7 +38,9 @@ for arch in $ARCHS; do
         *)
                 echo "Building opencore-amr for iPhoneSimulator $arch *****************"
                 PATH=`xcodebuild -version -sdk iphonesimulator PlatformPath`"/Developer/usr/bin:$PATH" \
-                CXX="xcrun --sdk iphonesimulator clang++ -arch $arch $IOSMV -miphoneos-version-min=5.0" \
+		SDKROOT=$(xcrun --sdk iphonesimulator --show-sdk-path)
+		CC="$(xcrun --sdk iphonesimulator -f clang)"
+		CXX="xcrun --sdk iphonesimulator clang++ -arch $arch $IOSMV -miphoneos-version-min=5.0" \
                 ./configure \
                 --prefix=$DEST \
                 --disable-shared
